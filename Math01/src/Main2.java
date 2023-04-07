4b27f5b1e4c6370f684b274634e2dbc99d5aa56c
nayoon <nayoonk928@gmail.com>
2023-04-08 06:00:21 +0900

    약수와 소수

--- Math01/src/Main2.java
+++ Math01/src/Main2.java
@@ -1,2 +1,25 @@
+import java.util.ArrayList;
+
+/*
+    1부터 30까지의 숫자 중 소수 찾기
+ */
 public class Main2 {
+    public static void main(String[] args) {
+        boolean isPrime;
+
+        for (int i = 2; i <= 30; i++) {
+            isPrime = true;
+
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                System.out.print(i + " ");
+            }
+        }
+    }
 }
